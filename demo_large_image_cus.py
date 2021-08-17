ad9d57aeeca7d9364ded65a578f0c86f205f0104
qilei123 <qileimail123@gmail.com>
2021-08-17 17:02:43 -0400

    test

--- demo_large_image_cus.py
+++ demo_large_image_cus.py
@@ -246,7 +246,8 @@ def single_video_process(model,cap1,cap2,frame_size=(540,960),dst_dir=None):
         tmer.save_results(dst_dir+".pkl")
 def videos_process(src_dir,dst_dir):
     print("start initial detector")
-    model_name = "ReDet_re50_refpn_1x_TD_ms_3cat"
+    #model_name = "ReDet_re50_refpn_1x_TD_ms_3cat"
+    model_name = "retinanet_obb_r50_fpn_2x_TD"
     model = DetectorModel(
         r"configs/ReDet_trans_drone/"+model_name+".py",
         r"/data2/qilei_chen/DATA/trans_drone/work_dirs/"+model_name+"/latest.pth")
