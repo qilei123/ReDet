48d6aaff5fb154ae546241100985bd53c8bc302b
qilei123 <qileimail123@gmail.com>
2021-09-22 22:35:22 -0400

    test

--- release_gpu.py
+++ release_gpu.py
@@ -1,19 +1,30 @@
 import os
 import time
- 
+import argparse
  
 def get_exec_out(sxcute_str):
     out_list = os.popen(sxcute_str).readlines()
     return out_list
- 
- 
+
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-k", "--keywords", type=str,default=None)
+    args = parser.parse_args()
+
+    keywords = "test"
+
+    if args.keywords==None:
+        print("no keywords")
+    else:
+        keywords = args.keywords
+
     excute_str = 'nvidia-smi'
     out_list = get_exec_out(excute_str)
     #print(out_list)
     for oo in out_list:
         #print(oo)
-        if oo.find('redet/bin/python') != -1:
+        if oo.find(keywords) != -1:
             # split()函数默认可以按空格分割，并且把结果中的空字符串删除掉，留下有用信息
             proc_list = oo.split()
             pid = proc_list[4].strip()
